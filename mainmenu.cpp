e484985c8b92d282aac01d025978943712e1ed95
Ethan <35935079+Tendooh@users.noreply.github.com>
2022-03-10 07:18:21 -0800

    Update mainmenu.cpp

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -18,7 +18,7 @@ int main()
 	int check = 0;
 
 	//welcome message
-	cout << "\n\nWelcome to the CS300 Contact and Device Manager\nCreated by Ethan Saftler\n\n\n";
+	cout << "\n\nWelcome to the Chocoholics Terminal Manager\n\n\n";
 
 	//while loop for main menu using switch statement for selection
 	while (selection != 'Z')
