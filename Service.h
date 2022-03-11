3665fdebb59878da34a79be677ded3d425a663e5
ChentaoMa <93690199+ChentaoMa@users.noreply.github.com>
2022-03-11 10:47:29 -0800

    Update Service.h

--- Service.h
+++ Service.h
@@ -1,12 +1,24 @@
 #include <iostream>
 #include "BasicDefinition.h"
 
+struct Service
+{
+    char* service_name;
+	int service_code;
+	char* provided_date;
+	char* logged_date;
+	int memberID;
+	int providerID;
+	float service_fee;
+	char* comments;
+};
+
 class Service
 {
 public:
 	Service();
-	int getService();
-	Service(char*, int, char*, char*, int, int, float, char*);
+	getService();
+	Service(char* se_name, int code, char* pr_date, char* lo_date, int memID, int proID, float fee, char * se_comments);
 	Service(const Service* To_Add);
 	~Service();
 
