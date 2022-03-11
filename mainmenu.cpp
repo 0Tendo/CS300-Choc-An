679fceec8dcfbef47cfa266177f8b4e391781a68
ChentaoMa <93690199+ChentaoMa@users.noreply.github.com>
2022-03-11 03:34:20 -0800

    Update mainmenu.cpp

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -66,7 +66,7 @@ int main()
 			}
 				break;
 			case '2':
-				Service * new_service = new Service;
+				Service new_service;
 				new_service->getService();
 				cout << "\n\nPress enter to return to the main menu...\n";
 				cin.get(); 
